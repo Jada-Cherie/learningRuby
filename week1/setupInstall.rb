d610a5d77e975572cfd28fe5842e1a5b33e03eaa
Jada-Cherie <jada.c.townsend@gmail.com>
2023-08-10 00:03:03 -0400

    study notes

---
+++ week1/setupInstall.rb
@@ -0,0 +1,20 @@
+# INSTALLATION
+# 
+# For a Mac:
+# Ruby is automaticallly installed on MacIOs machines. To check, open your  terminal and run the following command to check which version of Ruby you have installed:
+# 'ruby -v' this should output the version of Ruby you have on the macIos
+
+# For a Windows machine, you have to download Ruby from here: 
+# https://rubyinstaller.org/downloads/ 
+# and follow the instructions in this freecodecamp ruby video, installation starts at (1:24) for the Windows Installation, https://www.youtube.com/watch?v=t_ispmWmdjY&t=4095s 
+
+# You will need a code editor: I'm using VS code
+
+# You could install a Ruby Version Manager (RVM) to manage multiple versions of Ruby on your machine. (I did not) You can use this tutorial: https://nrogap.medium.com/install-rvm-in-macos-step-by-step-d3b3c236953b
+
+# Running Ruby Code in VS Code
+
+#  To run Ruby code in VSCode, open up a new file and save it with the .rb extension.
+# To run the code, you can open up a new terminal in VSCode and run the following command:
+# `ruby <filename>.rb`
+# You can also run the code directly in the editor by pressing the play button in the top right corner of the editor window.
