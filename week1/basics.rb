23c1b8cc57f047eb1b6b1ca6bcb14c25b53262c4
Jada-Cherie <jada.c.townsend@gmail.com>
2023-08-10 00:24:10 -0400

    notes

---
+++ week1/basics.rb
@@ -0,0 +1,21 @@
+
+# RUBY
+
+
+# Ruby is a powerful, flexible programming language you can use in web/Internet development, to process text, to create games, and as part of the popular Ruby on Rails web framework. Ruby is:
+
+# High-level: meaning reading and writing Ruby is really easy—it looks a lot like regular English!
+
+# Interpreted: meaning you don’t need a compiler to write and run Ruby. You can write it here at Codecademy or even on your own computer (many are shipped with the Ruby interpreter built in—we’ll get to the interpreter later in this lesson).
+
+# Object-oriented: meaning it allows users to manipulate data structures called objects in order to build and execute programs. We’ll learn more about objects later, but for now, all you need to know is everything in Ruby is an object.
+
+# Easy to use: Ruby was designed by Yukihiro Matsumoto (often just called “Matz”) in 1995. Matz set out to design a language that emphasized human needs over those of the computer, which is why Ruby is so easy to pick up.
+
+# Ruby is a dynamic, interpreted, object-oriented, high-level programming language that was created for developer happinness and was created in 1995 to emphasize the needs of humans over computer. This makes the language reads like almost english. 
+# Interpreted means that you do not need a compiler to write or run Ruby, you can write it in your terminal and it will run.
+# Ruby is case sensitive and cares about capitalizations 
+# to print variables with Ruby you use "puts" or "print" commands; the puts command adds a new line after the output, while the print command does not
+# Single line comments are written with a hashtag (#) and multiline comments are written with =begin and =end
+
+#DATA TYPES AND VARIABLES
